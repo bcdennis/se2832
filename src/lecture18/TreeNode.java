2726ebff2a48c97ccaec3be41bd09f9e8fe44e40
Brad Dennis <bcdennis@gmail.com>
2016-05-10 09:19:32 -0500

    adding in TreeNode for mutation testing

---
+++ src/lecture18/TreeNode.java
@@ -0,0 +1,125 @@
+/**
+ * SE2832
+ * Spring 2016
+ * Week 9
+ */
+package lecture18;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Represents the tree node.
+ *
+ * @param <E> the type of values stored in the node.
+ * @author Brad Dennis, Ph.D.
+ * @version 1.0
+ */
+public class TreeNode<E> {
+    private E value;
+    private boolean hasParent;
+    private List<TreeNode<E>> children;
+
+
+    /**
+     * Constructs the TreeNode.
+     *
+     * @param value the value to store in the node.
+     */
+    public TreeNode(E value) {
+        this.value = value;
+        children = new ArrayList<>();
+    }
+
+    /**
+     * Adds a child to the tree node.
+     *
+     * @param child the child to add.
+     */
+    public void addChild(TreeNode<E> child) {
+
+        if (child == null) {
+            throw new IllegalArgumentException("Child cannot be null.");
+        }
+
+        if (child.hasParent) {
+            throw new IllegalArgumentException("Child already has a parent.");
+        }
+
+        child.hasParent = true;
+        children.add(child);
+    }
+
+    /**
+     * Returns the height of the tree.
+     *
+     * @return the height of the tree.
+     */
+    public int height() {
+        return preorder(this, 0, 0);
+    }
+
+    // this node is visited before child nodes.
+    private int preorder(TreeNode<E> node, int height, int maxHeight) {
+        height++;
+        System.out.println(node.value);
+
+        if (maxHeight < height) {
+            maxHeight  = height;
+        }
+
+        if(node.children.size() == 0) {
+            return maxHeight;
+        }
+
+        for(TreeNode<E> child : node.children) {
+            maxHeight = preorder(child, height, maxHeight);
+        }
+
+        return maxHeight;
+    }
+
+    /**
+     * Traverses the tree in depth first order.
+     *
+     * @return returns a list of values in DF order.
+     */
+    public List<E> depthFirst() {
+        List<E> values = new ArrayList<>();
+
+        Stack<TreeNode<E>> stack = new Stack<>();
+        stack.push(this);
+
+        while (!stack.isEmpty()) {
+            TreeNode<E> node = stack.pop();
+            values.add(node.value);
+            for (TreeNode<E> t : node.children) {
+                stack.push(t);
+            }
+        }
+
+        return values;
+    }
+
+    public static void main(String[] args) {
+        TreeNode<String> a = new TreeNode<>("A");
+        TreeNode<String> b = new TreeNode<>("B");
+        TreeNode<String> c= new TreeNode<>("C");
+        TreeNode<String> d = new TreeNode<>("D");
+        TreeNode<String> e = new TreeNode<>("E");
+        TreeNode<String> f = new TreeNode<>("F");
+        TreeNode<String> g = new TreeNode<>("G");
+
+        b.addChild(c);
+        b.addChild(d);
+        e.addChild(f);
+        f.addChild(g);
+
+        a.addChild(e);
+        a.addChild(b);
+
+
+        System.out.println(a.depthFirst());
+    }
+}
---
+++ tests/lecture18/TreeNodeTest.java
@@ -0,0 +1,30 @@
+/**
+ * SE2832
+ * Spring 2016
+ * Week 9
+ */
+package lecture18;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Tests the tree node class.
+ *
+ * @author Brad Dennis, Ph.D.
+ * @version 1.0
+ */
+public class TreeNodeTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
